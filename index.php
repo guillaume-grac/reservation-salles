29b02e3ccd05607cf302ae4b98741af0ffd89740
Guillaume <gui.grc@yahoo.com>
2020-12-15 17:05:26 +0100

    Resa-salles : Debut

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title> | Accueil</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>  
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
