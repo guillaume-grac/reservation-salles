25ba88969d58291acd45288b0ec2ef27329bb30b
Guillaume <gui.grc@yahoo.com>
2020-12-16 15:10:27 +0100

    Res. Salle : En cours :@

--- index.php
+++ index.php
@@ -2,20 +2,62 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title> | Accueil</title>
+    <title>Loc'In | Accueil</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 </head>
 <body>
     <header>
-
+        <nav class="mb-1 navbar navbar-expand-lg lighten-1">
+            <section class="collapse navbar-collapse" id="navbarSupportedContent-555">
+                <ul class="navbar-nav membres">
+                    <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-users"></i> Inscription</a></li>
+                    <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-user-check"></i> Connexion</a></li>
+                    <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-user-cog"></i> Profil</a></li>
+                </ul>
+                <a class="navbar-brand title-princ" href="index.php"><b>Loc'In</b></a>
+                <ul class="navbar-nav ml-auto salles">
+                    <li class="nav-item"><a class="nav-link" href=""><i class="far fa-calendar-alt"></i> Planning</a></li>
+                    <li class="nav-item"><a class="nav-link" href=""><i class="far fa-calendar-plus"></i> Reserver</a></li>
+                    <li class="nav-item"><a class="nav-link" href=""><i class="far fa-calendar-check"></i> Les réservations</a></li>
+                </ul>
+            </section>
+        </nav>
     </header>
-    <main>  
-
+    <main>
+        <section class="scene">
+            <div class="card">
+                <div class="card_face ">
+                    <img src="images/salle1.1.jpg" alt ="salle1.1">
+                </div>
+                <div class="card_face card_faceback">
+                    <img src="images/salle1.2.jpg" alt ="salle1.2">
+                </div>
+            </div>
+            <div class="card">
+                <div class="card_face ">
+                    <img src="images/salle2.1.jpg" alt ="salle2.1">
+                </div>
+                <div class="card_face card_faceback">
+                    <img src="images/salle2.2.jpg" alt ="salle2.2">
+                </div>
+            </div>
+            <div class="card">
+                <div class="card_face">
+                    <img src="images/salle3.1.jpg" alt ="salle3.1">
+                </div>
+                <div class="card_face card_faceback">
+                    <img src="images/salle3.2.jpg" alt ="salle3.2">
+                </div>
+            </div>
+        </section>
+        <section class="presentation">
+            <h2>Loc'In vous propose différente salles insolites à louer ! </h2>
+        </section>
     </main>
     <footer>
-
     </footer>
 </body>
 </html>
+
