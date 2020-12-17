72d3ffd77ecf70558f28e75ab1f3e43b2771bc55
Guillaume <gui.grc@yahoo.com>
2020-12-17 16:20:40 +0100

    resa salle | Test

--- index.php
+++ index.php
@@ -2,62 +2,43 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Loc'In | Accueil</title>
+    <title>Loc'Sea | Accueil</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 </head>
 <body>
     <header>
-        <nav class="mb-1 navbar navbar-expand-lg lighten-1">
-            <section class="collapse navbar-collapse" id="navbarSupportedContent-555">
-                <ul class="navbar-nav membres">
-                    <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-users"></i> Inscription</a></li>
-                    <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-user-check"></i> Connexion</a></li>
-                    <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-user-cog"></i> Profil</a></li>
-                </ul>
-                <a class="navbar-brand title-princ" href="index.php"><b>Loc'In</b></a>
-                <ul class="navbar-nav ml-auto salles">
-                    <li class="nav-item"><a class="nav-link" href=""><i class="far fa-calendar-alt"></i> Planning</a></li>
-                    <li class="nav-item"><a class="nav-link" href=""><i class="far fa-calendar-plus"></i> Reserver</a></li>
-                    <li class="nav-item"><a class="nav-link" href=""><i class="far fa-calendar-check"></i> Les réservations</a></li>
-                </ul>
-            </section>
-        </nav>
+        <div class="header-anim"> <!-- $header_index_anim -->
+            <nav class="mb-1 navbar navbar-expand-lg lighten-1">
+                <section class="collapse navbar-collapse" id="navbarSupportedContent-555">
+                    <ul class="navbar-nav membres">
+                        <li class="nav-item"><a class="nav-link" href="php/inscription.php">| <i class="fas fa-users"></i> Inscription</a></li>
+                        <li class="nav-item"><a class="nav-link" href="php/connexion.php">| <i class="fas fa-user-check"></i> Connexion</a></li>
+                        <li class="nav-item"><a class="nav-link" href="php/profil.php">| <i class="fas fa-user-cog"></i> Profil</a></li>
+                    </ul>
+                    <a class="navbar-brand title-princ" href="index.php"><b>Loc'Sea</b></a>
+                    <ul class="navbar-nav ml-auto salles">
+                        <li class="nav-item"><a class="nav-link" href="php/planning.php">| <i class="far fa-calendar-alt"></i> Planning</a></li>
+                        <li class="nav-item"><a class="nav-link" href="php/reservation-form.php">| <i class="far fa-calendar-plus"></i> Reserver</a></li>
+                        <li class="nav-item"><a class="nav-link" href="php/reservation.php">| <i class="far fa-calendar-check"></i> Les réservations</a></li>
+                    </ul>
+                </section>
+            </nav>
+        </div>
     </header>
     <main>
-        <section class="scene">
-            <div class="card">
-                <div class="card_face ">
-                    <img src="images/salle1.1.jpg" alt ="salle1.1">
-                </div>
-                <div class="card_face card_faceback">
-                    <img src="images/salle1.2.jpg" alt ="salle1.2">
-                </div>
-            </div>
-            <div class="card">
-                <div class="card_face ">
-                    <img src="images/salle2.1.jpg" alt ="salle2.1">
-                </div>
-                <div class="card_face card_faceback">
-                    <img src="images/salle2.2.jpg" alt ="salle2.2">
-                </div>
-            </div>
-            <div class="card">
-                <div class="card_face">
-                    <img src="images/salle3.1.jpg" alt ="salle3.1">
-                </div>
-                <div class="card_face card_faceback">
-                    <img src="images/salle3.2.jpg" alt ="salle3.2">
-                </div>
-            </div>
-        </section>
-        <section class="presentation">
-            <h2>Loc'In vous propose différente salles insolites à louer ! </h2>
+        <section class="main-content container-fluid">
+            <h1>Avec Loc'Sea, vivez l'expèrience la plus immersive !</h1><br>
+            <p>Loc'Sea vous propose des locations de salles de luxe totalement immergées, hors du commun.<br>
+            Plusierus salles sont disponibles, chacune adaptées à un type d'évenement précis.<br>
+            N'hésitez plus, reservez votre salle et vivez une experience intense et merveilleuse.</p>
         </section>
     </main>
     <footer>
+        <section class="footer-copyright text-center py-3">
+            <p><b>© 2020 Copyright : Guillaume G.</b></p>
+        </section>
     </footer>
 </body>
 </html>
-
---
+++ php/connexion.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Loc'Sea | Connexion</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/connexion.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+</head>
+<body>
+    <header>
+        <nav class="mb-1 navbar navbar-expand-lg lighten-1">
+            <section class="collapse navbar-collapse" id="navbarSupportedContent-555">
+                <ul class="navbar-nav membres">
+                    <li class="nav-item"><a class="nav-link" href="php/inscription.php">| <i class="fas fa-users"></i> Inscription</a></li>
+                    <li class="nav-item"><a class="nav-link" href="php/connexion.php">| <i class="fas fa-user-check"></i> Connexion</a></li>
+                    <li class="nav-item"><a class="nav-link" href="php/profil.php">| <i class="fas fa-user-cog"></i> Profil</a></li>
+                </ul>
+                <a class="navbar-brand title-princ" href="../index.php"><b>Loc'Sea</b></a>
+                <ul class="navbar-nav ml-auto salles">
+                    <li class="nav-item"><a class="nav-link" href="php/planning.php">| <i class="far fa-calendar-alt"></i> Planning</a></li>
+                    <li class="nav-item"><a class="nav-link" href="php/reservation-form.php">| <i class="far fa-calendar-plus"></i> Reserver</a></li>
+                    <li class="nav-item"><a class="nav-link" href="php/reservation.php">| <i class="far fa-calendar-check"></i> Les réservations</a></li>
+                </ul>
+            </section>
+        </nav>
+    </header>
+    <main>
+        <section class="container-fluid">
+            <section class="loginBox">
+                <h1>Connexion</h1>
+                <form method="post" action="connexion.php">
+                    <section class="inputBox">
+                        <label id="label-style" for="login">Votre Login :</label>
+                        <input type="text" name="login" placeholder="Votre login" required>
+                    </section>
+                    <section class="inputBox">
+                        <label id="label-style" for="password">Votre mot de passe :</label>
+                        <input type="password" name="password" placeholder="Votre Mot de Passe" required>
+                    </section>
+                    <button type="submit" name="register" class="bouton btn btn-dark">Connexion</button>
+                </form>
+            </section>
+        </section>
+    </main>
+    <footer>
+        <section class="footer-copyright text-center py-3">
+            <p><b>© 2020 Copyright : Guillaume G.</b></p>
+        </section>
+    </footer>
+</body>
+</html>
